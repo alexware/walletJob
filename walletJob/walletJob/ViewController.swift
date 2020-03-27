8f2857ce1899326defd34ab37e2993879dc1e56d
Alex Ramos <ramos84@gmail.com>
2020-03-27 13:22:49 +0100

    first initial commit

---
+++ walletJob/walletJob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  walletJob
+//
+//  Created by Alejandro Ramos Grifé on 27/03/2020.
+//  Copyright © 2020 alexware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
