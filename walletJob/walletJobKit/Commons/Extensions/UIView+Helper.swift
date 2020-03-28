e831cb51bb7d4a6bd78e5434afdadd41f409c908
Alex Ramos <ramos84@gmail.com>
2020-03-28 21:28:01 +0100

    add podfile, organize project in folders and create xcconfig in each schemes

---
+++ walletJob/walletJobKit/Commons/Extensions/UIView+Helper.swift
@@ -0,0 +1,13 @@
+//
+//  UIView+Helper.swift
+//  walletJobKit
+//
+//  Created by Alejandro Ramos Grifé on 27/03/2020.
+//  Copyright © 2020 alexware. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+}
---
+++ walletJob/walletJobKit/walletJobKit.h
@@ -0,0 +1,19 @@
+//
+//  walletJobKit.h
+//  walletJobKit
+//
+//  Created by Alejandro Ramos Grifé on 27/03/2020.
+//  Copyright © 2020 alexware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for walletJobKit.
+FOUNDATION_EXPORT double walletJobKitVersionNumber;
+
+//! Project version string for walletJobKit.
+FOUNDATION_EXPORT const unsigned char walletJobKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <walletJobKit/PublicHeader.h>
+
+
