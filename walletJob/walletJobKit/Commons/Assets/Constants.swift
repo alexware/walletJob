0326b3e9abd0c3a03948c1173e8ea15c58eed2c4
Alex Ramos <alex.ramos@cornerjob.com>
2020-04-13 16:35:42 +0200

    new files to include constants and userDefaults

---
+++ walletJob/walletJobKit/Commons/Assets/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  walletJobKit
+//
+//  Created by Alejandro Ramos on 13/04/2020.
+//  Copyright © 2020 alexware. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+    
+    struct Idenfifiers {
+        static let bundle: String = "com.walletJob.app"
+        static let group: String = "com.walletJob"
+    }
+    
+}
