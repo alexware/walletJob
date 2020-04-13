f5789010916ac3a8d8a6c6383cdbbb1dd66f4f24
Alex Ramos <alex.ramos@cornerjob.com>
2020-04-13 13:37:24 +0200

    class to manage user defaults

--- walletJob/walletJobKit/DataSource/UserDefaults/UserDataManager.swift
+++ walletJob/walletJobKit/DataSource/UserDefaults/UserDataManager.swift
@@ -8,7 +8,28 @@
 
 import Foundation
 
-class WJUserDefaults: DataManager {
-    
-    
+class UserDataManager: DataManager {
+    func create<T>(_ model: T.Type, completion: @escaping ((T) -> Void)) throws where T : Storable {
+         //
+     }
+     
+     func save(object: Storable) throws {
+         //
+     }
+     
+     func update(object: Storable) throws {
+         //
+     }
+     
+     func delete(object: Storable) throws {
+         //
+     }
+     
+     func deleteAll<T>(_ model: T.Type) throws where T : Storable {
+         //
+     }
+     
+     func fetch<T>(_ model: T.Type, predicate: NSPredicate?, sorted: Sorted?, completion: (([T]) -> ())) where T : Storable {
+         //
+     }
 }
