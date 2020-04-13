f3377691c39660e1010420c483cad1ca07540897
Alex Ramos <alex.ramos@cornerjob.com>
2020-04-13 13:36:03 +0200

    create new loginview controller

--- walletJob/walletJob/Features/Login/LoginViewController.swift
+++ walletJob/walletJob/Features/Login/LoginViewController.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 alexware. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+    }
+    
+    func setupLayout() {
+        self.view.backgroundColor = UIColor.white
+    }
+
+    func dismiss() {
+        self.dismiss(animated: true) {
+        }
+    }
+    
+}
