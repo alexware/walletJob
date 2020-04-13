8a49ecfecb60a349121e4990bb4343ed209d0ed0
Alex Ramos <alex.ramos@cornerjob.com>
2020-04-13 13:32:56 +0200

    create entities

--- walletJob/walletJobKit/Commons/Entities/User.swift
+++ walletJob/walletJobKit/Commons/Entities/User.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+enum Role {
+    case company
+    case candidate
+}
+
+struct GeoPoint {
+    var latitude: Double
+    var longitude: Double
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    func stringGeoPoint() -> String {
+        return "(\(latitude), \(longitude))"
+    }
+}
+
+struct User {
+    var id: String
+    var name: String?
+    var surname: String?
+    var location: GeoPoint
+    var role: Role
+}
