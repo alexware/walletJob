b016066e941fc0cd48146f79a069380e8e37efc3
Alex Ramos <alex.ramos@cornerjob.com>
2020-04-13 13:37:01 +0200

    class to manage coredata

--- walletJob/walletJobKit/DataSource/Persistence/CoreData/CoreDataManager.swift
+++ walletJob/walletJobKit/DataSource/Persistence/CoreData/CoreDataManager.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataManager: DataManager {
+    
+    let data: NSManagedObject?
+    
+    init(data: NSManagedObject?) {
+        self.data = data
+    }
+    
+    func create<T>(_ model: T.Type, completion: @escaping ((T) -> Void)) throws where T : Storable {
+         //
+     }
+     
+     func save(object: Storable) throws {
+         //
+     }
+     
+     func update(object: Storable) throws {
+         //
+     }
+     
+     func delete(object: Storable) throws {
+         //
+     }
+     
+     func deleteAll<T>(_ model: T.Type) throws where T : Storable {
+         //
+     }
+     
+     func fetch<T>(_ model: T.Type, predicate: NSPredicate?, sorted: Sorted?, completion: (([T]) -> ())) where T : Storable {
+         //
+     }
+}
